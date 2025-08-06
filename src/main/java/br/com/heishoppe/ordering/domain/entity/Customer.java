43a2cbd229eecd4ecdfb69d91482de4b3f690e6e
Fernando Dantas <fernandojrsud@hotmail.com>
2025-08-05 21:03:10 -0300

    Refinando domain model

---
+++ src/main/java/br/com/heishoppe/ordering/domain/entity/Customer.java
@@ -0,0 +1,21 @@
+package br.com.heishoppe.ordering.domain.entity;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public class Customer {
+
+    private UUID id;
+    private String fullName;
+    private LocalDate birthDate;
+    private String email;
+    private String phone;
+    private String document;
+    private Boolean promotionNotificationsAllowed;
+    private Boolean archived;
+    private OffsetDateTime registeredAt;
+    private OffsetDateTime archivedAt;
+    private Integer loyaltyPoints;
+
+}
